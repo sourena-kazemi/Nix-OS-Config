2f3b072ba6576f26d7aa2112945c52a6091bbe28
sourena-kazemi <kazemisourena@gmail.com>
2026-07-30 22:41:31 +0330

    config niri's input settings

--- home/configs/desktop/niri.nix
+++ home/configs/desktop/niri.nix
@@ -9,12 +9,22 @@
     input = {
       keyboard = {
         xkb.layout = "us";
+        repeat-delay = 250;
+        repeat-rate = 40;
       };
 
       touchpad = {
         tap = true;
         natural-scroll = false;
         dwt = true;
+        click-method = "button-areas";
+        accel-speed = 0.2;
+        scroll-factor = 1.0;
+        tap-button-map = "left-right-middle";
+      };
+
+      mouse = {
+        accel-speed = 0.0;
       };
     };
 
